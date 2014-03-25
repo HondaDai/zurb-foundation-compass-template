bf3fd75007692abb266b36997fa4994b999df63b
HondaDai <hondadai.tw@gmail.com>
2014-03-25 20:07:01 +0800

    use foundation cli to new project

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,9 @@
 
 
 echo "upgrade Zurb Foundation on templates/project/source..."
-rm -r ./templates/project/source
+rm -rf ./templates/project/source
 
-git clone https://github.com/zurb/foundation-compass-template.git ./templates/project/source
+# git clone https://github.com/zurb/foundation-compass-template.git ./templates/project/source
+foundation new templates/project/source
+rm -rf templates/project/source/.git
 
